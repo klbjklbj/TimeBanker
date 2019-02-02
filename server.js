4bc4f0b61516f5e4000f4937d7ee4e7e8b245bab
SNAFU <nholdsworth@me.com>
2019-02-01 23:03:18 -0800

    example seeds and schema for testing

--- server.js
+++ server.js
@@ -13,17 +13,17 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Handlebars
-app.engine(
-  "handlebars",
-  exphbs({
-    defaultLayout: "main"
-  })
-);
-app.set("view engine", "handlebars");
+// app.engine(
+//   "handlebars",
+//   exphbs({
+//     defaultLayout: "main"
+//   })
+// );
+// app.set("view engine", "handlebars");
 
 // Routes
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+// require("./routes/apiRoutes")(app);
+// require("./routes/htmlRoutes")(app);
 
 var syncOptions = { force: false };
 
