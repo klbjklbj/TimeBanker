24eab4f55a07a591cd467330d8da2e58f30fc78c
SNAFU <nholdsworth@me.com>
2019-02-03 22:42:00 -0800

    JSON routing to localhost:8080

--- server.js
+++ server.js
@@ -1,15 +1,27 @@
+// dotenv allows for sensitive information to be stored securely
 require("dotenv").config();
+// express is a javascript library for implementing servers in Node
 var express = require("express");
+
+// handlebars is a templating engine
 // var exphbs = require("express-handlebars");
 
+// setting a variable names app to hold the function express()
+var app = express();
+
+// This sets up the port so that it can be assigned by Heroku or listen on port8080 by default
+var PORT = process.env.PORT || 8080;
+
+// this is for sequilize.  Sequilize will then syn our models with the database i.e. create our schemas and seeds
 var db = require("./models");
 
-var app = express();
-var PORT = process.env.PORT || 3000;
 
-// Middleware
+
+// Middleware so that express will handle data parsing
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// this sets up express to be able to use files from a directory that the client downloads to use with the app.  These are known as "static" directories.
 app.use(express.static("public"));
 
 // Handlebars
@@ -22,9 +34,10 @@ app.use(express.static("public"));
 // app.set("view engine", "handlebars");
 
 // Routes
-// require("./routes/apiRoutes")(app);
+require("./routes/apiRoutes")(app);
 // require("./routes/htmlRoutes")(app);
 
+// TODO:This has something to do with resetting the database during testing?  See code below...
 var syncOptions = { force: true };
 
 // If running a test, set syncOptions.force to true
@@ -33,8 +46,24 @@ if (process.env.NODE_ENV === "test") {
   syncOptions.force = true;
 }
 
-// Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(function() {
+// Sequelize is taking our models files and rendering the data as directed by the code in those files (user.js) instead of us building the code in mysql, THEN once the schema has been run and the table created based on the definitions in the models folder, we are adding in some seed data to the User table.  THEN, once the seeds have been planted into the schema: start the server listening (for get and post requests from the localhost.) ------------------------------------/
+db.sequelize.sync(syncOptions).then(() => {
+  db.User.create({
+    firstName: 'Leonard',
+    lastName: 'Nimoy',
+    skill: 'Logic and Mind Melding',
+    email: 'spock@vulcan.com',
+    image: 'https://66.media.tumblr.com/c8945ee30829cb081e4c2eeaca115b16/tumblr_plxye0xXPy1w314t0o1_540.png'
+  });
+  db.User.create({
+    firstName: 'William',
+    lastName: 'Shatner',
+    skill: 'Captain',
+    email: 'kirk@enterprise.com',
+    image: 'https://www.gettyimages.com/detail/news-photo/canadian-actor-william-shatner-as-captain-james-t-kirk-in-news-photo/81792246'
+  });
+
+}).then(function() {
   app.listen(PORT, function() {
     console.log(
       "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
