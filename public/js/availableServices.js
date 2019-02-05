ab2ad7a3623f6879385d194234e34c2d4a49e80f
SNAFU <nholdsworth@me.com>
2019-02-05 00:37:16 -0800

    so close to having the media objects dynamically rendered

--- public/js/availableServices.js
+++ public/js/availableServices.js
@@ -1,9 +1,11 @@
+// When the document is ready run the code inside this function
 $(document).ready(function () {
-
+    // creating a vairable to reference the <ul> on line 42 in the available_services.html 
     let skillDiv = $("#skillBank");
 
+    // this is a variable to hold all of the ajax methods in an object 
     let API = {
-
+        // this function is an ajax request to get all the data from the given route
         getUsers: function () {
             return $.ajax({
                 url: "api/users",
@@ -12,12 +14,42 @@ $(document).ready(function () {
         }
 
     };
-
+    // This runs the getUsers function, after the user data has been retrieved... TODO:dynamically create a new media object to display each user and their skills
     API.getUsers().then(function (userData) {
-        let $users = userData.map(function (currentUser) {
+        // create a variable called allUsers and using ,map() create a new array with all of the user objects in it and assign them to the appropriate html elements
+        let allUsers = userData.map(function (currentUser) {
+
             console.log(currentUser);
+
+            let skill = $(`<p>`)
+                .text(currentUser.skill);
+
+            let name = $(`<h5 class="mt-0 mb-1">`)
+                .text(currentUser.name);
+
+            let mediaBody = $(`<div class="media-body">`)
+                .append(name, skill);
+
+            let mediaImage = $(`<img class="mr-3" alt="...">`)
+                .attr({
+                    src: currentUser.image
+                });
+
+            let mediaObject = $(`<li class="media" data-toggle="modal" data-target="#emailModal" data-whatever="user@mail">`)
+                .append(mediaImage, mediaBody);
+
+            let mediaContainer = $(`<ul class="list-unstyled" id="skillBank">`)
+                .append(mediaObject);
+
+            let mediaDestination = $(`#centerBody`)
+                .append(mediaContainer);
+
         })
     })
+
+
+
+
 
 });
 
